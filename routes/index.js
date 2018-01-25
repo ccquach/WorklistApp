7cf2a4c253bf01a7ead363ab4f405559ba362557
cynthia.quach <ccquach93@gmail.com>
2018-01-25 09:20:21 -0800

    add forgot post route

--- routes/index.js
+++ routes/index.js
@@ -67,4 +67,62 @@ router.get("/forgot", function(req, res) {
 	res.render("forgot");
 });
 
+router.post("/forgot", function(req, res, next) {
+	async.waterfall([
+		// create token
+		function(done) {
+			crypto.randomBytes(20, function(err, buf) {
+				var token = buf.toString("hex");
+				done(err, token);
+			});
+		},
+		// look for provided email
+		function(token, done) {
+			User.findOne({ email: req.body.email }, function(err, user) {
+				if(!user) {
+					req.flash("error", "No account with that email address exists.");
+					return res.redirect("/forgot");
+				}
+
+				user.resetPasswordToken = token;
+				user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+				user.save(function(err) {
+					done(err, token, user);
+				});
+			});
+		},
+		// send reset email
+		function(token, user, done) {
+			// set service
+			var smtpTransport = nodemailer.createTransport({
+				service: "Gmail",									// TODO: update with Outlook + credentials
+				auth: {
+					user: process.env.GMAIL_EMAIL,
+					pass: process.env.GMAIL_PASS
+				}
+			});
+			// compose email
+			var mailOptions = {
+				to: user.email,
+				from: process.env.GMAIL_EMAIL,						// TODO: update with work email
+				subject: "Worklist Password Reset",
+				text: "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+					  "Please click on the following link, or paste this into your browser to complete the process:\n\n" +
+				  	  "http://" + req.headers.host + "/reset/" + token + "\n\n" +
+				  	  "If you did not request this, please ignore this email and your password will remain unchanged.\n"
+			};
+			// send email
+			smtpTransport.sendMail(mailOptions, function(err) {
+				console.log("Password reset email sent to " + user.email);
+				req.flash("success", "An e-mail has been sent to " + user.email + " with further instructions.");
+				done(err, "done");
+			});
+		}
+	], function(err) {
+		if(err) return next(err);
+		res.redirect("/forgot");
+	});
+});
+
 module.exports = router;
