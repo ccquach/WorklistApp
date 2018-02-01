75305c6b8cbe50a1221f348b5917c661ee68cb42
cynthia.quach <ccquach93@gmail.com>
2018-01-31 20:48:56 -0800

    hide/show sections based on page nav item selected

--- public/scripts/show.js
+++ public/scripts/show.js
@@ -1,6 +1,42 @@
-function setActiveItem() {
-	$("div.secondary.menu > a").on("click", function() {
-		$("a").removeClass("active");
-		$(this).addClass("active");
-	});
+$(document).ready(function() {
+	$("#commercial-section").hide();
+	$("#medical-section").hide();
+	$("#log-section").hide();
+	showActiveSection();
+});
+
+function showActiveSection() {
+	var menu = $(".ui.secondary.pointing.menu a");
+	for(var i = 0; i < menu.length; i++) {
+		$(menu[i]).on("click", function() {
+			for(var j = 0; j < menu.length; j++) {
+				$(menu[j]).removeClass("active");
+			}
+			// highlight active menu item
+			$(this).addClass("active");
+
+			// hide sections
+			$("#patient-section").hide();
+			$("#commercial-section").hide();
+			$("#medical-section").hide();
+			$("#log-section").hide();
+			// show selected section
+			switch($(this).text()) {
+				case "Patient":
+					$("#patient-section").show();
+					break;
+				case "Commercial":
+					$("#commercial-section").show();
+					break;
+				case "Medi-cal":
+					$("#medical-section").show();
+					break;
+				case "Communication Log":
+					$("#log-section").show();
+					break;
+				default:
+					break;
+			}
+		});
+	}
 }
