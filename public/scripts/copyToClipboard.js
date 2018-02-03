a9df1deacf3f57274d622d92c729e96d3dd9a793
cynthia.quach <ccquach93@gmail.com>
2018-02-03 12:18:46 -0800

    add copy to clipboard function to comment builder element

---
+++ public/scripts/copyToClipboard.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	copyToClipboard();	
+});
+
+function copyToClipboard() {
+	const targets = document.querySelectorAll(".copy-target");
+	for(var i = 0; i < targets.length; i++) {
+		const target = targets[i];
+		target.onclick = function() {
+		  document.execCommand("copy");
+		}
+
+		target.addEventListener("copy", function(event) {
+		  event.preventDefault();
+		  if (event.clipboardData) {
+		    event.clipboardData.setData("text/plain", target.textContent);
+		    // console.log(event.clipboardData.getData("text"));
+		  }
+		});
+	}
+}
